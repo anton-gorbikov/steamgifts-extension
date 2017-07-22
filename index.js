a7c950d1da4d94e853ec6fd37523579af025b572
Anton Gorbikov <anton.gorbikov@gmail.com>
2017-07-22 17:01:04 +0300

    Add buttons for entering giveaway.

--- index.js
+++ index.js
@@ -4,12 +4,33 @@
 // @version      1.0
 // @description  script for extending steamgifts.com UI.
 // @author       Anton Gorbikov
-// @match        https://steamgifts.com
+// @match        https://www.steamgifts.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
 	'use strict';
 
-	// Your code here...
+	const TEMPLATE = '' +
+		'<div data-do="entry_insert" class="sidebar__entry-insert">' +
+			'<i class="fa fa-plus-circle"></i>' +
+			'Enter Giveaway' +
+			'<span class="sidebar__entry__points"></span>' +
+		'</div>';
+	const URL = 'ajax.php';
+
+	function getToken() {
+		return $('[name=xsrf_token]').first().val();
+	}
+
+	function appendButtons() {
+		let elements = $('.giveaway__columns');
+
+		elements.each(function() {
+			let button = $(TEMPLATE)[0];
+			this.append(button);
+		});
+	}
+
+	appendButtons();
 })();
